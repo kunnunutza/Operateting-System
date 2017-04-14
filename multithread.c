3f8f51445cc2c6fdbe06e79348e93a699270223e
kunnunutza <kunnunutza@hotmail.com>
2017-04-14 14:37:12 +0700

    Update multithread.c

--- multithread.c
+++ multithread.c
@@ -1,4 +1,4 @@
-/Nutthanicha Yongpoomputta 5888090
+//Nutthanicha Yongpoomputta 5888090
 //Inthukorn Limpachaveng 5888261
 // section 1
 
@@ -6,12 +6,31 @@
 #include<stdlib.h>
 #include<string.h>
 #include <unistd.h>
+#include <pthread.h>
 
 int main(int argc, char *argv[]){
-    int pro,con,size // #of producer,#of consumer,stack size
+    int pro,con,size,num[100]; // #of producer,#of consumer,stack size,element in input file,
     pro = atoi(argv[1]);
     con = atoi(argv[2]);
     size = atoi(argv[3]);
+    //file input
+    
+    //number of producer and consumer will not greater than 100
+    if(){
+        printf("incorrect!");
+        exit(0);
+    }
+    //multithread method
+    else{
+        
+        
+        
+        
+        
+    }
+    
+    
+   
 
 
 }
