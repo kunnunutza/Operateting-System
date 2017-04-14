c02adca2804e81da8bf6c92dd0c3aab91604826c
kunnunutza <kunnunutza@hotmail.com>
2017-04-14 19:47:35 +0700

    Update multithread.c

--- multithread.c
+++ multithread.c
@@ -1,17 +1,32 @@
-/Nutthanicha Yongpoomputta 5888090
+//Nutthanicha Yongpoomputta 5888090
 //Inthukorn Limpachaveng 5888261
 // section 1
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+
+
+void Producer(void * param){
+    
+}
+void Consumer(void * param){
+    
+}
 
 int main(int argc, char *argv[]){
-    int pro,con,size // #of producer,#of consumer,stack size
+    int pro,con,size,num[100],i; // #of producer,#of consumer,stack size,element in input file,
     pro = atoi(argv[1]);
     con = atoi(argv[2]);
     size = atoi(argv[3]);
-
-
+   
+    //file input
+    
+  
+    //multithread method
+      
+        
 }
+
