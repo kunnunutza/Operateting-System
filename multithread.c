4b7c5861ffcb38d5f89e1f498cf28af7534924f6
kunnunutza <kunnunutza@hotmail.com>
2017-04-15 07:27:51 +0700

    Update multithread.c

--- multithread.c
+++ multithread.c
@@ -1,4 +1,4 @@
-/Nutthanicha Yongpoomputta 5888090
+//Nutthanicha Yongpoomputta 5888090
 //Inthukorn Limpachaveng 5888261
 // section 1
 
@@ -6,12 +6,35 @@
 #include<stdlib.h>
 #include<string.h>
 #include <unistd.h>
+#include<pthread.h>
+#define BUFFER_SIZE 100
 
 int main(int argc, char *argv[]){
-    int pro,con,size // #of producer,#of consumer,stack size
+    int pro,con,size,num[100],n,i // #of producer,#of consumer,stack size
     pro = atoi(argv[1]);
     con = atoi(argv[2]);
     size = atoi(argv[3]);
 
+    printf("DONE");
+    exit(0);
 
 }
+
+//Producer method
+/*void Producer(void *param) {
+       while(true){
+       
+
+      }
+
+      pthread_exit(NULL);
+}
+
+//Consumer method
+
+void Consumer(void *param) {
+       
+
+     
+     pthread_exit(NULL);
+}*/
